c2a0cf5178dcd607b84c5c73f62eee332adad6e5
yhg0337 <58197935+yhg0337@users.noreply.github.com>
2020-07-28 01:33:15 +0900

    버그 픽스

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import axios from "axios";
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "/" : "https://210.179.121.232:80";
 ReactDOM.render(
   <React.StrictMode>
     <App />
